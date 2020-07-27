ce4c23b8ab1641136aa79bc263ffc7f0c9502a52
Dariy <waku108@gmail.com>
2020-07-27 15:36:49 +0300

    avtomate currency rate

--- app/Services/CurrencyConvertion.php
+++ app/Services/CurrencyConvertion.php
@@ -3,17 +3,37 @@
 namespace App\Services;
 
 use App\Currency;
+use Illuminate\Support\Facades\Http;
 
 class CurrencyConvertion
 {
+    protected static $webCurrency;
+
+    public static function loadCurrency()
+    {
+        if (is_null(self::$webCurrency)) {
+            self::$webCurrency = self::getEurRate();
+        }
+        return self::$webCurrency;
+    }
+    protected static function getEurRate()
+    {
+        $response = Http::get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+        return  json_decode($response, true)[1]['sale'];
+    }
+
     public static function convert($sum, $originCurrencyCode = 'EUR', $targetCurrenceCode = null)
     {
         $originCurrency = Currency::ByCode($originCurrencyCode)->first();
-        if (is_null($targetCurrenceCode)) {
+        self::loadCurrency();
+        if (!is_null(self::$webCurrency)) {
+            $rate = self::$webCurrency;
+        } elseif (is_null($targetCurrenceCode)) {
             $targetCurrenceCode = session('currency', 'EUR');
+            $targetCurrence = Currency::byCode($targetCurrenceCode)->first();
+            $rate =  $targetCurrence->rate;
         }
-        $targetCurrence = Currency::byCode($targetCurrenceCode)->first();
 
-        return $sum / 0.75 * $targetCurrence->rate;
+        return $sum / 0.75 * $rate;
     }
 }
