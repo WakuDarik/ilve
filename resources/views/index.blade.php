7ba0b036dd406b1423353a4f07e9905c0f129af0
Dariy <waku108@gmail.com>
2020-11-20 09:13:58 +0200

    h1 fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,16 +11,16 @@
     style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.2)), to(rgba(0, 0, 0, 0.2))), url('{{Storage::url('static/ILVE_Home_Hero-2048x1152.jpg')}}'); background-image:linear-gradient(180deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('{{Storage::url('static/ILVE_Home_Hero-2048x1152.jpg')}}');">
     <div data-w-id="3352ee9f-aac6-4690-27b2-90a01d48c6f1" class="pay-off oriz">
         <div class="content-title">
-            <h1 class="white _1">
-                Житти, </h1>
+            <h3 class="white _1 h3_like_h1">
+                Житти, </h3>
         </div>
         <div class="content-title">
-            <h1 class="white _1" data-ix="title-2">
-                Готувати, </h1>
+            <h3 class="white _1 h3_like_h1" data-ix="title-2">
+                Готувати, </h3>
         </div>
         <div class="content-title">
-            <h1 class="white _1" data-ix="title-3">
-                Любити. </h1>
+            <h3 class="white _1 h3_like_h1" data-ix="title-3">
+                Любити. </h3>
         </div>
     </div>
     <div id="scroll-hide" class="div-block-33">
@@ -32,7 +32,7 @@
     style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.2)), to(rgba(0, 0, 0, 0.2))), url('{{Storage::url('static/cover_2.jpg')}}'); background-image:linear-gradient(180deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('{{Storage::url('static/cover_2.jpg')}}');">
     <div class="pay-off">
         <div class="content-title">
-            <h1 class="white">ILVE: компания</h1>
+            <h1 class="white">ILVE</h1> <span class="white h3_like_h1">компания</span>
         </div>
         <a href="/company" class="button white w-inline-block" data-ix="button">
             <div>Про нас</div>
@@ -46,7 +46,7 @@
     style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.2)), to(rgba(0, 0, 0, 0.2))), url('{{Storage::url('static/ILVE_Home_Forno400_A-2048x1152.jpg')}}'); background-image:linear-gradient(180deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('{{Storage::url('static/ILVE_Home_Forno400_A-2048x1152.jpg')}}');">
     <div class="pay-off">
         <div class="content-title">
-            <h1 class="white">Піч для піци 400°</h1>
+            <h2 class="white h3_like_h1">Піч для піци 400°</h2>
         </div>
         <a href="/forno-pizza-400" class="button white w-inline-block" data-ix="button">
             <div>Дізнайтеся більше</div>
@@ -60,7 +60,7 @@
     style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.2)), to(rgba(0, 0, 0, 0.2))), url('{{Storage::url('static/ILVE_Home_CCSTAR_PP_SS-2048x1152.jpg')}}'); background-image:linear-gradient(180deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('{{Storage::url('static/ILVE_Home_CCSTAR_PP_SS-2048x1152.jpg')}}');">
     <div class="pay-off">
         <div class="content-title">
-            <h1 class="white">«Зіркова Колона»: готує, як шеф-кухар</h1>
+            <h2 class="white h3_like_h1">«Зіркова Колона»: готує, як шеф-кухар</h2>
         </div>
         <a href="/colonna-stellata" class="button white w-inline-block" data-ix="button">
             <div>Дізнайтеся більше</div>
@@ -74,7 +74,7 @@
     style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.2)), to(rgba(0, 0, 0, 0.2))), url('{{Storage::url('static/ILVE_Home_Ultracombi-2048x1152.jpg')}}'); background-image:linear-gradient(180deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('{{Storage::url('static/ILVE_Home_Ultracombi-2048x1152.jpg')}}');">
     <div class="pay-off">
         <div class="content-title">
-            <h1 class="white">Ультракомбіновані: три духовки в одній</h1>
+            <h2 class="white h3_like_h1">Ультракомбіновані: три духовки в одній</h2>
         </div>
         <a href="/ultracombi" class="button white w-inline-block" data-ix="button">
             <div>Дізнайтеся більше</div>
@@ -87,7 +87,7 @@
 <div class="block-product hide-on-mobile-device"
     style="background-image: url('{{Storage::url('static/ILVE_Home_Configuratore-1024x576.png')}}');">
     <div class="block-configuration">
-        <h2 class="serif white">Свобода у створенні досконалої <br />
+        <h2 class="serif white h3_like_h1">Свобода у створенні досконалої <br />
             плити для вашої кухні</h2>
         <a href="/range-cookers?conf=t" class="button white w-inline-block" data-ix="button">
             <div>Створіть Вашу плиту</div>
